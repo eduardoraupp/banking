068ce6ae394db1ed035846abdd66aeadef812c79
Eduardo Raupp <rauppduh@gmail.com>
2017-03-19 20:24:16 +0100

    commit role

--- src/main/java/com/banking/domain/security/Role.java
+++ src/main/java/com/banking/domain/security/Role.java
@@ -4,9 +4,6 @@ import org.springframework.security.core.GrantedAuthority;
 
 public class Role implements GrantedAuthority {
 
-	/**
-	 *
-	 */
 	private static final long serialVersionUID = 1L;
 	private final String roleName;
 
